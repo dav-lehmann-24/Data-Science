1826ad370183f71e2d5f5552820e6c17bef5260f
dav-lehmann-24 <191463061+dav-lehmann-24@users.noreply.github.com>
2025-09-12 17:22:20 +0200

    Adjustments to the code and option to play again

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -18,12 +18,10 @@ def checkWinner(board, symbol):
         (0, 4, 8), (2, 4, 6)
     ]
     for (a, b, c) in wins:
-        print(f"Checking positions {a}, {b}, {c} for symbol {symbol}")
+        # print(f"Checking positions {a}, {b}, {c} for symbol {symbol}")
         if board[a] == board[b] == board[c] == symbol:
-            print("YES")
+            # print("YES")
             return True
-        else:
-            print("NO")
     return False
 
 # Main game
@@ -32,35 +30,62 @@ def main():
     Player1 = "X"
     Player2 = "O"
     currentPlayer = Player1
+    active_game = True
 
     turns = 0
-    while turns < 9:
-        printBoard(board)
+    while active_game:
+        while turns < 9:
+            printBoard(board)
 
-        try:
-            move = int(input("Player " + currentPlayer + ", enter your move (1-9): "))
-            if move < 1 or move > 9:
+            try:
+                move = int(input("Player " + currentPlayer + ", enter your move (1-9): "))
+                if move < 1 or move > 9:
+                    print("Invalid move. Try again.")
+                    continue
+            except ValueError:
+                print("Invalid input. Please enter a number between 1 and 9.")
+                continue
+
+            if board[move-1] == "X" or board[move-1] == "O":
                 print("Invalid move. Try again.")
                 continue
-        except ValueError:
-            print("Invalid input. Please enter a number between 1 and 9.")
-            continue
+            else:
+                board[move-1] = currentPlayer
+                turns += 1
 
-        if board[move-1] == "X" or board[move-1] == "O":
-            print("Invalid move. Try again.")
+                if checkWinner(board, currentPlayer):
+                    printBoard(board)
+                    print("Player " + currentPlayer + " wins!")
+                    response = input("Do you want to play again? (y/n): ").strip().lower()
+                    if response == 'y':
+                        board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+                        currentPlayer = Player1
+                        turns = 0
+                        continue
+                    elif response == 'n':
+                        print("Thanks for playing!")
+                        active_game = False
+                        break
+                    else:
+                        print("Invalid input. Exiting game.")
+                        active_game = False
+                        break
+
+                currentPlayer = Player2 if currentPlayer == Player1 else Player1
+
+        if not active_game:
+            break
+
+        printBoard(board)
+        print("It's a draw!")
+        response = input("Do you want to play again? (y/n): ").strip().lower()
+        if response == 'y':
+            board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+            currentPlayer = Player1
+            turns = 0
             continue
         else:
-            board[move-1] = currentPlayer
-            turns += 1
-
-            if checkWinner(board, currentPlayer):
-                printBoard(board)
-                print("Player " + currentPlayer + " wins!")
-                return
-
-            currentPlayer = Player2 if currentPlayer == Player1 else Player1
-
-    printBoard(board)
-    print("It's a draw!")
+            print("Thanks for playing!")
+            active_game = False
 
 main()
